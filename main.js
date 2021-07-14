578e4ec995b22ca81b919d6cbdbe289948894c51
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2021-07-13 20:26:29 -0400

    Add working buttons and feedback

--- main.js
+++ main.js
@@ -19,21 +19,64 @@ const scoreBoard = document.querySelector(".score");
 const points = document.querySelector(".points");
 const startButton = document.querySelector(".startButton");
 const stopButton = document.querySelector(".stopButton");
+const fasterButton = document.querySelector(".fasterButton");
+const slowerButton = document.querySelector(".slowerButton");
+const faster = document.querySelector(".faster");
 
-// The y-velocity of the computer paddle
-let computerPaddleYPosition = 0;
-
-//Starting position for the player paddle.
+//Starting position for the player & computer paddle.
 let playerPaddleYPosition = 200;
+let computerPaddleYPosition = 200;
+
 playerPaddle.style.top = `${playerPaddleYPosition}px`;
 
 //This value determines how the player paddle moves. When the 's' key is pressed this value becomes '1'. When the
 //'w' key is pressed, the value is -1.
 let playerPaddleYVelocity = 0;
 
+let superBall = false;
+
+let timer = 0;
+
+let fastTemp = "";
+
+let fastTemp2 = "";
+
+//Player control for the player paddle. Using keydown so you can hold the key, to get continuous movement.
+addEventListener("keydown", (playerInput) => {
+  if (playerInput.key === "s") {
+    playerPaddleYVelocity = 1;
+  }
+
+  if (playerInput.key === "w") {
+    playerPaddleYVelocity = -1;
+  }
+
+  if (playerInput.key === "l") {
+    superBall = true;
+    console.log("Superball is", superBall);
+  }
+});
+
+//Keyup will set the velocity to 0. The update function will always try to 'move' the player paddle, but without
+//a keydown press, it will always be adding or subracting 0 from the yPos.
+addEventListener("keyup", (playerInput) => {
+  if (playerInput.key === "s") {
+    playerPaddleYVelocity = 0;
+  }
+
+  if (playerInput.key === "w") {
+    playerPaddleYVelocity = 0;
+  }
+
+  if (playerInput.key === "l") {
+    superBall = false;
+    console.log("Superball is", superBall);
+  }
+});
+
 //Position for the ball.
-let xPos = 0;
-let yPos = 0;
+let xPos = 240;
+let yPos = 340;
 
 //Speed of the ball, also doubles as a difficulty slider.
 let yVel = 1.25;
@@ -41,12 +84,7 @@ let xVel = 1.25;
 
 //Score of the game.
 let playerScore = 0;
-let compScore = -1;
-
-//Status of the game state; running vs not-running.
-let running = false;
-
-
+let compScore = 0;
 
 //
 //
@@ -54,51 +92,124 @@ let running = false;
 //
 //
 
+// window.onload = alert(
+//   "Hello! Welcome to Pong, the best Pong clone on the market."
+// );
 
-//Event listeners
-// startButton.addEventListener("click", startPush);
-// stopButton.addEventListener("click", stopPush);
-
-
-
-// const runner = setInterval(update, 5);
-
-// //Button controls
-// function startPush () {
-//   // setInterval(update, 5);
-// }
-
-// function stopPush () {
-//   clearInterval(runner);
-// }
-
-let runner = '';
+//Start & Stop controls
+//This variable holds the setInterval & clearInterval window methods
+let runner = "";
 
 startButton.onclick = startGame;
+stopButton.onclick = stopGame;
 
 function startGame() {
+  //This first return case is to prevent mulitple clicks. I found that if you run the setInterval method
+  //more than once, it's additive and you will effectively double the rate at which it is called. This code
+  //will check if the game is already 'running' or not.
+  if (runner !== "") {
+    return;
+  }
+  runner = setInterval(update, 5);
 
-  if (runner !== '') {
-    return
+  playerPaddleYPosition = 200;
+}
+
+function stopGame() {
+  clearInterval(runner);
+  runner = "";
+}
+
+//Game speed controls
+fasterButton.onclick = speedUp;
+slowerButton.onclick = slowDown;
+
+function speedUp() {
+  yVel = yVel * 1.2;
+  xVel = xVel * 1.2;
+
+  fastImg();
+  setTimeout(outFast, 3000);
+  return
+}
+
+function slowDown() {
+  yVel = yVel * 0.8;
+  xVel = xVel * 0.8;
+}
+
+function fastImg() {
+  fastTemp = setInterval(fastFade, 50);
+  return;
+}
+
+function outFast() {
+  fastTemp = setInterval(fastFadeOut, 50);
+
+  return;
+}
+
+function fastFade() {
+  let fasterOpacity = window
+    .getComputedStyle(faster)
+    .getPropertyValue("opacity");
+  let fasterOpacityNum = Number(fasterOpacity);
+
+  if (fasterOpacityNum < 1) {
+    console.log("I am INCREASING the opacity of the image.");
+    fasterOpacityNum = fasterOpacityNum + 0.08;
+    let input = fasterOpacityNum.toString();
+    faster.style.opacity = input;
+    return;
   }
 
-  runner = setInterval(update, 5);
+  if (fasterOpacityNum >= 1) {
+    console.log("The image is at maximum opacity.");
+    clearInterval(fastTemp);
+    fastTemp = "";
+    return;
+  }
+
+
+
+  return;
+}
+
+function fastFadeOut() {
+
+  let fasterOpacity = window
+    .getComputedStyle(faster)
+    .getPropertyValue("opacity");
+  let fasterOpacityNum = Number(fasterOpacity);
+
+  if (fasterOpacityNum > 0) {
+    console.log("I am DECREASING the opacity of the image.");
+    fasterOpacityNum = fasterOpacityNum - 0.08;
+    let input = fasterOpacityNum.toString();
+    faster.style.opacity = input;
+    return;
+  }
+
+  if (fasterOpacityNum <= 0) {
+    console.log("The image is at minimum opacity.");
+    clearInterval(fastTemp);
+    fastTemp = "";
+    return;
+  }
 
 }
 
+function fastStop() {
+  console.log("We should be stopping");
 
-
-stopButton.onclick = stopGame;
-
-function stopGame() {
-
-  clearInterval(runner);
-  runner = '';
-
+  fastTemp = "";
+  console.log(fastTemp);
 }
 
 // Update the pong world
 function update() {
+  timer++;
+
   // Update the computer paddle's position to follow the ball.
   let follower = yPos - 50;
   computerPaddleYPosition = yPos;
@@ -137,6 +248,8 @@ function update() {
 
     ball.style.top = `${yPos}px`;
     ball.style.left = `${xPos}px`;
+
+    stopGame();
   }
 
   //Paddle ball bounce
@@ -146,14 +259,27 @@ function update() {
     yPos <= computerPaddleYPosition + 100
   ) {
     xVel = xVel * -1;
+    ball.style.backgroundColor = "green";
   }
 
   if (
+    superBall === false &&
     xPos <= 20 &&
     yPos >= playerPaddleYPosition &&
     yPos <= playerPaddleYPosition + 100
   ) {
     xVel = xVel * -1;
+  }
+
+  if (
+    superBall === true &&
+    xPos <= 20 &&
+    yPos >= playerPaddleYPosition &&
+    yPos <= playerPaddleYPosition + 100
+  ) {
+    xVel = xVel * -1;
+
+    ball.style.backgroundColor = "orange";
   }
 
   //Update the position of the ball.
@@ -165,37 +291,10 @@ function update() {
   ball.style.left = `${xPos}px`;
 
   //Update the scoreboard.
-  let pointOutput = playerScore + ' - ' + compScore;
+  let pointOutput = playerScore + " - " + compScore;
   points.innerText = pointOutput;
-
-
-
-
-
-
-
 }
 
-//Player control for the player paddle.
-addEventListener("keydown", (playerInput) => {
-  if (playerInput.key === "s") {
-    playerPaddleYVelocity = 1;
-  }
+// faces.fadeTo("slow", 0);
 
-  if (playerInput.key === "w") {
-    playerPaddleYVelocity = -1;
-  }
-});
-
-addEventListener("keyup", (playerInput) => {
-  if (playerInput.key === "s") {
-    playerPaddleYVelocity = 0;
-  }
-
-  if (playerInput.key === "w") {
-    playerPaddleYVelocity = 0;
-  }
-});
-
-// Call the update() function every 35ms.
-//   setInterval(update, 5);
+// faces.style.backgroundColor = 'black';
